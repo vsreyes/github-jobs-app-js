33be4dc7b7e69f5526555ce80de08ce15c28b027
vsreyes <virgilio.s.reyes@gmail.com>
2020-10-14 21:38:52 +0200

    Corrections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import useFetchJobs from './useFetchJobs'
-import { Container } from 'react-bootstrap'
-import Job from './Job'
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
+import Job from './Job';
 import JobsPagination from './JobsPagination';
 import SearchForm from './SearchForm';
 
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <Container className="my-4">
-      <h1 className="mb-4">Github Jobs</h1>
+      <h1 className="mb-4">GitHub Jobs</h1>
       <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
